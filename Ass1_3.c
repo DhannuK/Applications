f43fa92e7a95005b717aec79b13929f7a9557b28
DhannuK <129723723+DhannuK@users.noreply.github.com>
2023-07-21 21:40:47 +0530

    Applications

---
+++ Ass1_3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int Mode = 0;
+	
+	if(argc != 3)
+	{
+		printf("Invalid number of arguments. \n");
+		return -1;
+	}
+	
+	if(strcmp(argv[2], "read") == 0)
+	{
+		Mode = R_OK;
+	}
+	else if(strcmp(argv[2], "write") == 0)
+	{
+		Mode = W_OK;
+	}
+	else if(strcmp(argv[2], "execute") == 0)
+	{
+		Mode = X_OK;
+	}
+	
+	if(access(argv[1], Mode) < 0)
+	{
+		printf("Unable to access %s file in %s Mode \n",argv[1],argv[2]);
+	}
+	else
+	{
+		printf("You can access %s file in %s mode \n",argv[1],argv[2]);
+	}	
+
+	 return 0;
+}
---
+++ Ass5_2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+
+#pragma pack(1)
+struct Student
+{
+	int Rollno;
+	char Sname[10];
+	float Marks;
+	int Age;
+};
+
+int main(int argc, char *argv[])
+{
+	int fd = 0;
+	char Fname[20];
+	struct Student sobj;
+	
+	printf("Enter the file name : \n");
+	scanf("%s",Fname);
+	
+	fd = open(Fname, O_RDONLY);
+	
+	read(fd,&sobj,sizeof(sobj));
+	
+	printf("Roll number : %d \n",sobj.Rollno);
+	printf("Name : %s \n",sobj.Sname);
+	printf("Marks : %f \n",sobj.Marks);
+	printf("Age : %d \n", sobj.Age);
+	
+	close(fd);
+
+	return 0;
+}
